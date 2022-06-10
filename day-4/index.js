447d02d784f0390325eedc71f49705b7be764c5e
Prabin Acharya <acharyaprabin101@gmail.com>
2022-06-10 22:19:06 +0545

    Day-4 part-1 challenge:Giant Squid

---
+++ day-4/index.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "./input.txt"), "utf-8")
+  .toString()
+  .trim()
+  .split("\n\n");
+
+const random_order = input.shift().split(",").map(Number);
+
+const boards = input.reduce((acc, board_str, i) => {
+  let board = [];
+
+  let rows = board_str.split("\n");
+  rows.forEach((row) =>
+    board.push(
+      row
+        .split(" ")
+        .filter((a) => a != "")
+        .map((b) => parseInt(b, 10))
+    )
+  );
+  acc.push(board);
+  return acc;
+}, []);
+
+function checkBoard(board, nums) {
+  const size = board[0].length;
+  const hits = new Array(size * 2).fill(0);
+
+  let Bingo = false;
+  let sum_unmarked = 0;
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (nums.includes(board[i][j])) {
+        if (++hits[i] === size || ++hits[size + j] === size) {
+          Bingo = true;
+        }
+      } else {
+        sum_unmarked += board[i][j];
+      }
+    }
+  }
+
+  return [Bingo, sum_unmarked];
+}
+
+for (let i = 0; i < random_order.length; i++) {
+  let Bingo;
+  for (let j = 0; j < boards.length; j++) {
+    if (!boards[j]) {
+      continue;
+    }
+    [Bingo, sum_unmarked] = checkBoard(boards[j], random_order.slice(0, i));
+    if (Bingo) {
+      console.log(sum_unmarked * random_order[i - 1]);
+      break;
+    }
+  }
+  if (Bingo) {
+    break;
+  }
+}
